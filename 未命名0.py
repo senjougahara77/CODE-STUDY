6e0e8fd8b281c3bbc84efd752e7445bf3e610635
senjougahara77 <maohuangrun@outlook.com>
2020-07-16 00:46:13 +0800

    test

---
+++ 未命名0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 22:15:15 2020
+
+@author: maiheri
+"""
+
+
+#name = "ana"
+#silly = "hi" + (name + " ")*3
+#print(silly)
+x=15
+print(x)
+x_str=str(x)
+print(x_str)
