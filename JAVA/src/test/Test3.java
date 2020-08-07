0fef3449fc1afa8cec40a46af5e2b41cf5b15f23
senjougahara77 <maohuangrun@outlook.com>
2020-08-08 00:34:57 +0800

    2

--- JAVA/src/test/Test3.java
+++ JAVA/src/test/Test3.java
@@ -8,11 +8,11 @@ public class Test3 {
     {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        double sum = 0;
-        for (int i = 1; i<n; i++)
+        double sum = 0.0;
+        for (int i = 1; i<=n; i++)
         {
             sum+=1.0/i;
         }
-        System.out.println(sum);
+        System.out.printf("%.2f",sum);
     }
 }
