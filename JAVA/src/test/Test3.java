15697d67d08bd363d2faa8e08e316f0c59a74b23
senjougahara77 <maohuangrun@outlook.com>
2020-08-09 22:36:07 +0800

    1

--- JAVA/src/test/Test3.java
+++ JAVA/src/test/Test3.java
@@ -7,12 +7,36 @@ public class Test3 {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        double sum = 0.0;
-        for (int i = 1; i<=n; i++)
+//        int n = in.nextInt();
+//        double sum = 0.0;
+//        for (int i = 1; i<=n; i++)
+//        {
+//            sum+=1.0/i;
+//        }
+//        System.out.printf("%.2f",sum);
+        int[] numbers = new int [100];
+        int x = 0;
+        double sum = 0;
+        int cnt = 0;
+        x = in.nextInt();
+        while(x != -1)
         {
-            sum+=1.0/i;
+            numbers[cnt] = x;
+            sum += x;
+            cnt ++;
+            x = in.nextInt();
         }
-        System.out.printf("%.2f",sum);
+        if ( cnt > 0)
+        {
+            double ave = sum/cnt;
+            for (int i=1; i<=cnt; i++)
+            {
+                if (numbers[i]>=ave)
+                {
+                   System.out.println(numbers[i]);
+                }
+            }
+            System.out.println(sum/cnt);
+        }
     }
 }
